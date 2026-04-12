86d63c0d8a26ff78fc9b7385fcfad6403f7fbbbd
Alex <lpzalejandroonlaptop@hotmail.com>
2026-04-12 18:26:46 +0200

    WIP: Delete commented code

--- src/app/alarm.spec.ts
+++ src/app/alarm.spec.ts
@@ -4,15 +4,6 @@ import { Temporal } from "@js-temporal/polyfill";
 
 describe('Alarm', () => {
 
-    // it.each(
-    //     [[new Temporal.PlainTime(9), new Temporal.PlainTime(9)],
-    //     [new Temporal.PlainTime(8), new Temporal.PlainTime(8)]])
-    //     ('should create object', (n, expected) => {
-    //         let sut = new Alarm(n)
-            
-    //         expect(sut.scheduledTime).toStrictEqual(expected);
-    //     })
-
     it('should create object' , () => {
         //Arrange & Act
         let sut = new Alarm(new Temporal.PlainTime(9), 2 , 5);
