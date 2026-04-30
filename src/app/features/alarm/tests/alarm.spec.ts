bd7f9faefa78faba17dd49872114754148210859
Alex <lpzalejandroonlaptop@hotmail.com>
2026-04-30 19:00:48 +0200

    WIP: Refactor interval deletion test

--- src/app/features/alarm/tests/alarm.spec.ts
+++ src/app/features/alarm/tests/alarm.spec.ts
@@ -37,23 +37,8 @@ describe('Alarm', () => {
     });
 
     it('should delete last interval added', () => {
-
-        //Arrange
-        const mockCallback = vi.fn();
-        const mockedTimeProvider: TimeProvider = {
-            now: Temporal.PlainDateTime.from(
-                {
-                    year: 2026,
-                    month: 4,
-                    day: 1,
-                    hour: 8,
-                    minute: 50,
-                    second: 0
-                }
-            )
-        };
-        
-        let sut = new Alarm(new Temporal.PlainTime(9), { count: 2, timeBetween: 5 }, mockCallback, mockedTimeProvider);
+       //Arrange & Act
+        let sut = AlarmMother.withCount(2, vi.fn());
 
         //Act
         sut.deleteInterval();
